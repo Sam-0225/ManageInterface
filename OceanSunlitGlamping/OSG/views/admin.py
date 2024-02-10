cbad78d62cf77f0c9b6ebd399e10570832a1b4e4
jifan <samuel951753@gmail.com>
2024-02-11 06:04:53 +0800

    build search function

--- OceanSunlitGlamping/OSG/views/admin.py
+++ OceanSunlitGlamping/OSG/views/admin.py
@@ -4,7 +4,12 @@ from OSG import models
 
 def admin_list(request):
     """管理員列表"""
-    queryset = models.Admin.objects.all()
+    data_dict = {}
+    value = request.GET.get('q')
+    if value is not None:
+        data_dict['username__contains'] = value
+
+    queryset = models.Admin.objects.filter(**data_dict)  # .order_by('-id')遞減排序
     context = {
         'queryset': queryset
     }
