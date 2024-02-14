4d6b1d58ca67e1cdca7010664b8191c0c6c0e29f
jifan <samuel951753@gmail.com>
2024-02-14 22:41:59 +0800

    git refactor name

--- ManageInterface/ManageInterface/wsgi.py
+++ ManageInterface/ManageInterface/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OceanSunlitGlamping project.
+WSGI config for ManageInterface project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OceanSunlitGlamping.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ManageInterface.settings')
 
 application = get_wsgi_application()
